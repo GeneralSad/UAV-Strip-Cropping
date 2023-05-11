b5f00a59d4118868fd76dcb5fecbe40ab1b1e158
Jasper <j.vanes1@student.avans.nl>
2023-05-11 16:00:15 +0200

    [WIP] part of the Drone Movement package
    See class diagram for context

---
+++ UAV-App/Drone Movement/DroneMovementStateMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UAV_App.Drone_Movement
+{
+    public class DroneMovementStateMachine
+    {/*
+        public List<PatrolState> parentStateHistory;
+        public void switchState(IMovementState state)
+        {
+
+        }*/
+    }
+}
