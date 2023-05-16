3008dfc9bfb079bf67b0f8ceb6feba7efdbf2df3
Jasper <j.vanes1@student.avans.nl>
2023-05-16 13:42:05 +0200

    [FIX] Potentially fixed the SwapChainPanel problem
    There was a problem that the UWP SwapChainPanel can't be used in WPF. Fixed this by installing the WPF control package and using that SwapChainPanel

---
+++ UAV-Strip-cropping-wpf/Drone Communication/DroneCommunicationSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_App.Drone_Communication
+{
+    public class DroneCommunicationSender
+    {
+        public bool sendMessage()
+        {
+            return false;
+        }
+    }
+}
---
+++ UAV-Strip-cropping-wpf/Drone Communication/DroneStreamReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_App.Drone_Communication
+{
+    public class DroneStreamReceiver : IStreamCommands
+    {
+        public void getLastFrame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
