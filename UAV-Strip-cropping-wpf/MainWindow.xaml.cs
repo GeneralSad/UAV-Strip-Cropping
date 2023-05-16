242bca1ff3053d3e72160ec25acf169a88db1682
Leon van der Sar <leon.v.d.sar@gmail.com>
2023-05-16 14:38:13 +0200

    [Add] Stuff and things. Didn't work

--- UAV-Strip-cropping-wpf/MainWindow.xaml.cs
+++ UAV-Strip-cropping-wpf/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+//using DJI.WindowsSDK;
+
 namespace UAV_Strip_cropping_wpf
 {
     /// <summary>
@@ -22,13 +24,56 @@ namespace UAV_Strip_cropping_wpf
         public MainWindow()
         {
             InitializeComponent();
+            //DJISDKManager.Instance.SDKRegistrationStateChanged += Instance_SDKRegistrationEvent;
+
+            //Replace with your registered App Key. Make sure your App Key matched your application's package name on DJI developer center.
+            //DJISDKManager.Instance.RegisterApp("20c067ae1fe8582f566f5e77");
         }
+
+        //private async void Instance_SDKRegistrationEvent(SDKRegistrationState state, SDKError resultCode)
+        //{
+        //    if (resultCode == SDKError.NO_ERROR)
+        //    {
+        //        //    System.Diagnostics.Debug.WriteLine("Register app successfully.");
+
+        //        //    //The product connection state will be updated when it changes here.
+        //        //    DJISDKManager.Instance.ComponentManager.GetProductHandler(0).ProductTypeChanged += async delegate (object sender, ProductTypeMsg? value)
+        //        //    {
+        //        //        await Dispatcher.InvokeAsync(System.Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+        //        //        {
+        //        //            if (value != null && value?.value != ProductType.UNRECOGNIZED)
+        //        //            {
+        //        //                System.Diagnostics.Debug.WriteLine("The Aircraft is connected now.");
+        //        //                //You can load/display your pages according to the aircraft connection state here.
+        //        //            }
+        //        //            else
+        //        //            {
+        //        //                System.Diagnostics.Debug.WriteLine("The Aircraft is disconnected now.");
+        //        //                //You can hide your pages according to the aircraft connection state here, or show the connection tips to the users.
+        //        //            }
+        //        //        });
+        //        //    };
+
+        //        //    //If you want to get the latest product connection state manually, you can use the following code
+        //        //    var productType = (await DJISDKManager.Instance.ComponentManager.GetProductHandler(0).GetProductTypeAsync()).value;
+        //        //    if (productType != null && productType?.value != ProductType.UNRECOGNIZED)
+        //        //    {
+        //        //        System.Diagnostics.Debug.WriteLine("The Aircraft is connected now.");
+        //        //        //You can load/display your pages according to the aircraft connection state here.
+        //        //    }
+        //        //}
+        //        //else
+        //        //{
+        //        //    System.Diagnostics.Debug.WriteLine("Register SDK failed, the error is: ");
+        //        //    System.Diagnostics.Debug.WriteLine(resultCode.ToString());
+        //    }
+        //}
 
         private void sidebar_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var selected = sidebar.SelectedItem as NavButton;
 
-            navframe.Navigate(selected.Navlink);
+            navframe.Navigate(selected?.Navlink);
 
         }
     }
