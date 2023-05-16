480fbf184eb84980c9036f762a65568cb75fbcdc
Jasper <j.vanes1@student.avans.nl>
2023-05-16 11:38:19 +0200

    [ADD] added the empty classes of the AI package
    Need to implement the logic of the AI package

---
+++ UAV-App/AI/AnimalDetection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAV_App.AI
+{
+    public class AnimalDetection : IAICallback
+    {
+        public enum AnimalType
+        {
+            HARMFUL,
+            OTHER
+        }
+
+        public List<AnimalType> processPrediction()
+        {
+            return null;
+        }
+
+        //TODO: change return type to an image, don't know the data type atm
+        public void prepareImage()
+        {
+            
+        }
+
+        public void notifyCallback()
+        {
+
+        }
+
+        public void startPrediction()
+        {
+
+        }
+    }
+}
